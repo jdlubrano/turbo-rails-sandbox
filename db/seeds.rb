e741a22c676bac52a22b21c9caadb2a141e5761e
Joel Lubrano <jlubrano@wrapbook.com>
2024-08-28 17:12:26 -0400

    Moar Messages in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-work_inbox = Inbox.create_or_find_by!(name: "Work")
+puts "Seeding Inboxes..."
+inboxes = %w[Work Personal].map { |name| Inbox.create_or_find_by!(name: name) }
 
-if work_inbox.messages.none?
-  1.upto(5) do |i|
-    work_inbox.messages.create!(title: "Message #{i}", body: "This is the content of message ##{i}")
-  end
+puts "Seeding Messages..."
+1.upto(50) do |i|
+  title = "Message #{i}"
+  next if Message.where(title: title).exists?
+
+  inbox = inboxes[i % inboxes.size]
+  inbox.messages.create!(title: title, body: "This is the content of message ##{i}")
 end
-
-personal_inbox = Inbox.create_or_find_by!(name: "Personal")
